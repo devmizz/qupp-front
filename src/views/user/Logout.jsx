0357219b463c84478bf209cd72339537519b3549
devmizz <garamminchan@gmail.com>
2022-09-26 10:16:03 +0900

    temp

--- src/views/user/Logout.jsx
+++ src/views/user/Logout.jsx
@@ -1,10 +1,8 @@
 import { Navigate } from 'react-router-dom'
 
-import { setCookie } from '../../util/cookie';
 import { removeCookie } from '../../util/cookie';
 
 function Logout() {
-  // setCookie("token", "");
   removeCookie("token");
 
   return <Navigate to="/" replace />
