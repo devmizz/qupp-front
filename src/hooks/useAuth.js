02c9a6b90e0375ba528a45c80a9b7a61efacb604
devmizz <garamminchan@gmail.com>
2022-09-29 11:18:48 +0900

    FIX: eslint warning

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,16 +1,16 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
-import { TOKEN } from "../constants";
-import { getCookie, removeCookie } from "../util/cookie";
+import { useCallback, useEffect, useMemo, useState } from 'react';
+import { TOKEN } from '../constants';
+import { getCookie, removeCookie } from '../util/cookie';
 
 const useAuth = () => {
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState('');
   const isAuthenticated = useMemo(() => Boolean(token), [token]);
 
   const logout = useCallback(() => {
     if (isAuthenticated) {
       removeCookie(TOKEN);
     }
-  }, []);
+  }, [isAuthenticated]);
 
   useEffect(() => {
     const _token = getCookie(TOKEN);
