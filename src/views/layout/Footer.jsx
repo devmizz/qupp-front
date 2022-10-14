43f16072612230a0ec8aff04369fe346000c7835
devmizz <garamminchan@gmail.com>
2022-10-14 21:35:40 +0900

    FEAT: footer

--- src/views/layout/Footer.jsx
+++ src/views/layout/Footer.jsx
@@ -1,5 +1,41 @@
 import React from 'react';
 
 export default function Footer() {
-  return <></>;
+  return (
+    <footer className="relative py-10 flex items-center justify-evenly bg-gray-700 md:py-10">
+      <div className="flex text-gray-300">
+        <h1>QUPP</h1>
+      </div>
+      <div className="flex flex-col text-gray-300">
+        <h4>Question University Problem Platform</h4>
+        <h6 className="text-center font-bold">개발자</h6>
+        <div className="flex text-center justify-evenly">
+          <div className="m-auto">
+            <div className="flex">
+              <div className="m-1">팀장</div>
+              <div className="m-1">민찬기</div>
+            </div>
+            <div className="flex">
+              <div className="m-1">팀원</div>
+              <div className="m-1">김세은</div>
+            </div>
+            <div className="flex">
+              <div className="m-1">팀원</div>
+              <div className="m-1">송용호</div>
+            </div>
+          </div>
+          <div className="m-auto">
+            <div className="flex">
+              <div className="m-1">팀원</div>
+              <div className="m-1">임소망</div>
+            </div>
+            <div className="flex">
+              <div className="m-1">팀원</div>
+              <div className="m-1">정영환</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
